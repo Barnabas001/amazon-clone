270971946e93f2733e02e0cfe98372e16c256aee
Barnabas001 <117881010+Barnabas001@users.noreply.github.com>
2025-04-07 04:38:49 +0100

    upload all necessary folders

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,7 @@
+import { formatCurrency } from '../utils/money.js';
+
+describe('test suite: formatCurrency', () => {
+  it('converts cent into dollars', () => {
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+});
